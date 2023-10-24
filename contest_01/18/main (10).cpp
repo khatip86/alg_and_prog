9be542a579781678ed185ad8f5852e74525a731a
khatip86 <145030600+khatip86@users.noreply.github.com>
2023-10-25 00:40:57 +0300

    Add files via upload

---
+++ contest_01/18/main (10).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string action(int n){
+    string output;
+    while (n){
+        output += --n % 26 + 'A';
+        n /= 26;
+    }
+    reverse (output.begin(), output.end());
+    return output;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    string answer;
+    answer = action(n);
+    cout << answer;
+}
