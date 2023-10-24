b90fd287d710400b8e7ee3abde44043feabfc213
khatip86 <145030600+khatip86@users.noreply.github.com>
2023-10-25 00:29:30 +0300

    Add files via upload

---
+++ contest_01/12/main (5).go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main() {
+    var x int64
+    var c = 0
+    fmt.Scan(&x)
+    
+    for x > 1 {
+        if x % 2 == 0 {
+            x = x / 2 
+        } else {
+            x = 3 * x + 1
+        }
+        
+        c++
+    }  
+    
+    fmt.Println(c)
+}
