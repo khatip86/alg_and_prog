d0e2c50a91f358da4f6821c42bdbd68c17cf9484
khatip86 <145030600+khatip86@users.noreply.github.com>
2023-10-25 00:22:33 +0300

    Add files via upload

---
+++ contest_01/09/main (6).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b;
+    c = sqrt(pow(a, 2) + pow(b, 2));
+    cout << c << endl;
+}
